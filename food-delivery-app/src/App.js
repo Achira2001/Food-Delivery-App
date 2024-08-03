c6ceb521faa9ee7bac4ffebcb87a117997e5a414
Achira2001 <achira.20220024@iit.ac.lk>
2024-08-04 01:56:00 +0530

    update files

--- food-delivery-app/src/App.js
+++ food-delivery-app/src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div>
-      Hello World!
+
+  <main className='pb-14 lg:pb-0'>
+    <Navbar/>
+    <div className = 'pt-16'>
+      <Home/>
     </div>
-  );
+    <Footer/>
+  </main>
+
+  )
 }
 
 export default App;
