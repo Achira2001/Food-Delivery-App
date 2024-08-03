b98353f34d7442c145ee03eb461481536cca380d
Achira2001 <achira.20220024@iit.ac.lk>
2024-08-04 03:09:44 +0530

    update file

--- food-delivery-app/src/pages/Home.js
+++ food-delivery-app/src/pages/Home.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
 export default function Home() {
-  return (
-    <div>
-      Home
-    </div>
-  )
+    return (
+        <div>
+            <div className="card mt-3" style={{"width": "18rem", "maxheight":"360px"}}>
+                <img src="..." className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text"> This is some important text. </p>
+                        <div className = "container w-100">
+                            <select className='m-2 h-100 w-100 big success'>
+                                {Array.from(Array(6))}
+                            </select>
+                        </div>
+                        
+                    </div>
+            </div>
+        </div>
+    )
 }
