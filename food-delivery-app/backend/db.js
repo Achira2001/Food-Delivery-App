06a2410708e339c34d2e389a293da07934177865
Achira2001 <achira.20220024@iit.ac.lk>
2024-08-07 23:57:20 +0530

    update files

--- food-delivery-app/backend/db.js
+++ food-delivery-app/backend/db.js
@@ -1,10 +1,21 @@
 const mongoose = require('mongoose');
-    
-const mongoURI = ''
+require('dotenv').config();
 
-const mongoDB = () =>{
-    mongoose.conncect(mongoURI,(=>{
-        console.log("connected")
-    }))
-    
-}
+const mongoURI = process.env.MONGO_URI;
+
+const mongoDB = async () => {
+    try {
+        await mongoose.connect(mongoURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Connected to MongoDB");
+
+        const fetchedData = await mongoose.connection.db.collection("food_items").find({}).toArray();
+        console.log(fetchedData);
+    } catch (error) {
+        console.error("Error connecting to MongoDB", error);
+    }
+};
+
+module.exports = mongoDB;
--- food-delivery-app/backend/index.js
+++ food-delivery-app/backend/index.js
@@ -1,11 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 5000
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const mongoDB = require('./db');
+
+// Connect to MongoDB
+mongoDB();
+
+app.use(express.json());
+app.use('/api/', require('./Routes/CreateUser'));
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+    res.send('Hello World!');
+});
 
 app.listen(port, () => {
-    console.log('Example app listening on port %{port}')
-})
+    console.log(`Example app listening on port ${port}`);
+});
