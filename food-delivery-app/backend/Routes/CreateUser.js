991fe7e5bda22fc9273ea01fe3663e8018854ee5
Achira2001 <achira.20220024@iit.ac.lk>
2024-08-08 23:29:04 +0530

    update files

--- food-delivery-app/backend/Routes/CreateUser.js
+++ food-delivery-app/backend/Routes/CreateUser.js
@@ -17,4 +17,19 @@ router.post('/createuser', async (req, res) => {
     }
 });
 
+router.post('/createuser', async (req, res) => {
+    try {
+        await User.create({
+            name: req.body.name,
+            password: req.body.password,
+            email: req.body.email,
+            location: req.body.location,
+        });
+        res.json({ success: true });
+    } catch (error) {
+        console.error(error);
+        res.json({ success: false });
+    });
+
+
 module.exports = router;
