18e06f4825f9b724e88e9a0c9c8fe6a0b13bdac4
Achira2001 <achira.20220024@iit.ac.lk>
2024-08-24 02:52:48 +0530

    update files

--- food-delivery-app/src/App.js
+++ food-delivery-app/src/App.js
@@ -3,29 +3,29 @@ import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import Login from './pages/Login';
 import Home from './pages/Home';
-import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Signup from './pages/Signup';
 import { CartProvider } from './components/ContextReducer';
 import MyOrder from './pages/MyOrder';
 
 function App() {
   return (
-
     <CartProvider>
-    <Router>
-      <div>
-      <Navbar/>
-        <Routes>
-          <Route exact path='/' element={<Home/>}/>
-          <Route exact path='/login' element={<Login/>}/>
-          <Route exact path='/createUser' element={<Signup/>}/>
-          <Route exact path='/myOrder' element={<MyOrder/>}/>
-        </Routes>
-      <Footer/>
-      </div>
-    </Router>
+      <Router>
+        <div className="app-container">
+          <Navbar />
+          <div className="content-wrap">
+            <Routes>
+              <Route exact path='/' element={<Home />} />
+              <Route exact path='/login' element={<Login />} />
+              <Route exact path='/createUser' element={<Signup />} />
+              <Route exact path='/myOrder' element={<MyOrder />} />
+            </Routes>
+          </div>
+          <Footer />
+        </div>
+      </Router>
     </CartProvider>
-
   )
 }
 
